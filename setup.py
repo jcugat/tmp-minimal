60ec9d641516ce59d5f2f65a5f4163ba8d7af559
Josep Cugat <josep.cugat@skyscanner.net>
2021-02-19 10:10:00 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='minimal',
+    version='0.1',
+    description='minimal',
+    url='https://www.python.org',
+    author='Me',
+    packages=['minimal'],
+    zip_safe=False,
+)
